5884571b216819de33243480910f4885dbcac298
Muhammad Bravo <muhammadelbravo@gmail.com>
2023-05-23 07:51:41 +0100

    add https to url

--- src/state/api.js
+++ src/state/api.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://muhdbravo.pythonanywhere.com/",
+    baseUrl: "https://muhdbravo.pythonanywhere.com/",
     
 
 })
